3afdc55911a84b6b755a53077d5ee12b35174650
Pawel Pilarczyk <pawelpilarczyk1@gmail.com>
2022-06-06 16:47:20 +0200

    Creation of input Component

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import {View} from 'react-native';
-import {Button} from 'src/components/Button';
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
+import { Button } from 'src/components/Button';
+import { Input } from 'src/components/Input';
 
 export const App = () => {
+  const [text, setText] = useState('');
   return (
-    <View>
-      <Button title="Hello" type="default" icon="fire" />
-    </View>
+    <ScrollView style={styles.wrapper}>
+      <Input
+        text={text}
+        type="text"
+        label="Test"
+        onChangeText={t => setText(t)}
+        maxLenght={20}
+      />
+      <Button />
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    paddingHorizontal: 20,
+    flex: 1,
+  },
+});
--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,1 +1,4 @@
-export {default as FireIcon} from './fire.svg';
+export { default as FireIcon } from './fire.svg';
+export { default as ErrorIcon } from './error.svg';
+export { default as EyeOpened } from './eyeOpened.svg';
+export { default as EyeClosed } from './eyeClosed.svg';
